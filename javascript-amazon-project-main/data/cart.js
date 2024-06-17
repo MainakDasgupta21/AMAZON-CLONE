29a497852b15881ca21c77253219847146284539
Mainak Dasgupta <dasguptamainak37@gmail.com>
2024-06-17 20:47:27 +0530

    Create automated tests for addToCart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,21 +1,27 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart ;
 
-if(!cart){
-        cart = [{
-                productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity : 2 ,
-                deliveryOptionId : '1'
-        },{
-                productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d' ,   
-                quantity : 1,
-                deliveryOptionId : '2'
-        }];
+loadFromStorage();
+
+export function loadFromStorage() {
+        cart = JSON.parse(localStorage.getItem('cart'));
+
+        if (!cart) {
+                cart = [{
+                        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                        quantity: 2,
+                        deliveryOptionId: '1'
+                }, {
+                        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                        quantity: 1,
+                        deliveryOptionId: '2'
+                }];
+        }
 }
 
 
 
-function saveToStorage(){
-        localStorage.setItem('cart', JSON.stringify(cart)); 
+function saveToStorage() {
+        localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
@@ -31,7 +37,7 @@ export function addToCart(productId) {
         } else {
                 cart.push({
                         productId: productId,
-                        quantity: 1 ,
+                        quantity: 1,
                         deliveryOptionId: '1',
                 });
         }
@@ -39,21 +45,21 @@ export function addToCart(productId) {
         saveToStorage();
 }
 
-export function removeFromCart(productId){
+export function removeFromCart(productId) {
         const newCart = [];
 
         cart.forEach((cartItem) => {
-                if(cartItem.productId !== productId) {
+                if (cartItem.productId !== productId) {
                         newCart.push(cartItem);
                 }
         });
 
-        cart = newCart ;
+        cart = newCart;
 
         saveToStorage();
 }
 
-export function updateDeliveryOption(productId, deliveryOptionId){
+export function updateDeliveryOption(productId, deliveryOptionId) {
         let matchingItem;
         cart.forEach((cartItem) => {
                 if (productId === cartItem.productId) {
