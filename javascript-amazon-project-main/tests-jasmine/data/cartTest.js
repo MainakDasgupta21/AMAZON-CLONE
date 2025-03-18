a972696b3902dbc632a9bb5e88f3b21fc72d6004
Mainak Dasgupta <dasguptamainak37@gmail.com>
2025-03-18 22:32:28 +0530

    All tests Done

--- javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -1,20 +1,20 @@
-import { addToCart , cart , loadFromStorage } from '../../data/cart.js';
+import { addToCart, cart, loadFromStorage } from '../../data/cart.js';
 
 
-describe('test suite addToCart' , () => {
-        it('adds an existing product to the cart',()=>{
-                spyOn(localStorage , 'setItem');
-                spyOn(localStorage , 'getItem').and.callFake(()=>{
+describe('test suite addToCart', () => {
+        it('adds an existing product to the cart', () => {
+                spyOn(localStorage, 'setItem');
+                spyOn(localStorage, 'getItem').and.callFake(() => {
                         return JSON.stringify([
                                 {
-                                   productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6' ,
-                                   quantity : 1 ,
-                                   deliveryOptionId : '1'
+                                        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                                        quantity: 1,
+                                        deliveryOptionId: '1'
                                 }
                         ]);
                 });
 
-                
+
                 loadFromStorage();
 
 
@@ -26,16 +26,16 @@ describe('test suite addToCart' , () => {
                 expect(cart[0].quantity).toEqual(2);
         });
 
-        it('adds an new product to the cart' , () => {
+        it('adds an new product to the cart', () => {
 
-                spyOn(localStorage , 'setItem');
+                spyOn(localStorage, 'setItem');
 
 
-                spyOn(localStorage , 'getItem').and.callFake(()=>{
+                spyOn(localStorage, 'getItem').and.callFake(() => {
                         return JSON.stringify([]);
                 });
 
-                
+
                 loadFromStorage();
 
 
